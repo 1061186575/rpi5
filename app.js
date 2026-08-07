f86b83e51d182205716e8a89aff357a0c300ee89
zp <zhoup.top>
2026-08-07 16:59:58 +0800

    .env 文件配置项大写

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ const FileStore = require("session-file-store")(session);
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const { getLocalIP } = require("./utils");
+const envFilePath = path.join(__dirname, '.env');
 
 // config
 try {
-    process.loadEnvFile(path.join(__dirname, '.env'));
+    process.loadEnvFile(envFilePath);
 } catch (error) {
     if (error.code !== 'ENOENT') {
         throw error;
     }
 }
-const username = process.env.username || 'admin';
-const password = process.env.password || '123';
+const username = process.env.LOGIN_USERNAME;
+const password = process.env.LOGIN_PASSWORD;
 const port = process.env.PORT || 3000;
 console.log("服务已启动，http://localhost:" + port);
 console.log(`服务已启动，http://${getLocalIP()}:` + port);
@@ -28,12 +29,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// 和XX共用一个sessionsCache, 相互免登录
-let sessionConfig = {
+// 把登录信息保存到文件，避免每次重启以后都需要重新登录
+const sessionConfig = {
     store: new FileStore({
         path: 'sessionsCache'
     }),
-    secret: "www123", //配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密，加密后的字符串作为 session id的值 发送给浏览器。
+    secret: "NDUyY2E0MTg0NmYyOTViNzM1M2VhMmUwODAyYjcxNjU", // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密，加密后的字符串作为 session id 的值发送给浏览器。
     name: "rpi_sid", // 返回客户端的 key 的名称，默认为 connect.sid。
     resave: true, // 强制保存 session， 即使它并没有变化。默认为 true。
     saveUninitialized: false, // saveUninitialized 属性为 true 则无论你是否使用 Session，都默认直接分配一个session id 给客户端。为 false 则真正存数据的时候才会分配 session id 给客户端, 默认为 true。
@@ -52,6 +53,9 @@ app.use(session(sessionConfig)); // 使用session
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/login', function (req, res, next) {
+    if (!username || !password) {
+        return res.send({ code: 1, msg: `请先在 .env 文件设置用户名和密码` })
+    }
     if (req.body.user !== username || req.body.password !== password) {
         return res.send({ code: 1, msg: '登录失败' })
     }
